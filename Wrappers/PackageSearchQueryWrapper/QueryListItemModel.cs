e16cf7ea8f368e5ac6790448648b178fd34c42eb
Mccree Lee <auroraziling@gmail.com>
2024-08-23 17:38:31 +0800

    feat: merge PackageSearch from PipManager.Windows

---
+++ Wrappers/PackageSearchQueryWrapper/QueryListItemModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PipManager.Core.Wrappers.PackageSearchQueryWrapper;
+
+public class QueryListItemModel
+{
+    public required string Name { get; set; }
+    public required string Version { get; set; }
+    public required string Description { get; set; }
+    public required string Url { get; set; }
+    public DateTime UpdateTime { get; set; }
+}
