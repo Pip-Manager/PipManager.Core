e47da9995045295d0b48ad29852611ebe4cb645d
Mccree Lee <auroraziling@gmail.com>
2024-10-26 20:17:35 +0800

    fix: package version time order

--- src/PyPackage/Models/PackageInfo.cs
+++ src/PyPackage/Models/PackageInfo.cs
@@ -5,29 +5,11 @@ namespace PipManager.Core.PyPackage.Models;
 public class PackageInfo
 {
     [JsonPropertyName("releases")]
-    public Dictionary<string, List<PackageInfoRelease>>? Releases;
+    public Dictionary<string, List<PackageInfoRelease>>? Releases { get; init; }
 }
 
 public class PackageInfoRelease
 {
-    [JsonPropertyName("filename")]
-    public string? Filename;
-
     [JsonPropertyName("upload_time")]
-    public string? UploadTime;
-
-    [JsonPropertyName("digests")]
-    public PackageInfoDigest? Digests;
-}
-
-public class PackageInfoDigest
-{
-    [JsonPropertyName("blake2b_256")]
-    public string? Blake2B256;
-
-    [JsonPropertyName("md5")]
-    public string? Md5;
-
-    [JsonPropertyName("sha256")]
-    public string? Sha256;
+    public string? UploadTime { get; set; }
 }
