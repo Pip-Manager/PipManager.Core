cceb0741beac9d5e2ca0564e81e5c8aec7de471d
Mccree Lee <auroraziling@gmail.com>
2024-10-26 20:13:36 +0800

    perf: use service extensions

---
+++ src/Services/CoreServiceRegisterExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PipManager.Core.Services;
+
+public static class CoreServiceRegisterExtensions
+{
+    public static void AddHttpClient(this IServiceCollection services, string appVersion)
+    {
+        services.AddTransient(_ =>
+        {
+            var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.All }) { DefaultRequestVersion = HttpVersion.Version20 };
+            client.DefaultRequestHeaders.Add("User-Agent", $"PipManager/{appVersion}");
+            client.Timeout = TimeSpan.FromSeconds(6);
+            return client;
+        });
+    }
+}
