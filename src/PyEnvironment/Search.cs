545f13ec28bfeec7d16ef1561f891a2972b97f1c
Mccree Lee <auroraziling@gmail.com>
2024-11-01 21:19:07 +0800

    clean code

--- src/PyEnvironment/Search.cs
+++ src/PyEnvironment/Search.cs
@@ -8,14 +8,14 @@ public static class Search
 {
     public static EnvironmentModel? FindEnvironmentByIdentifier(string identifier)
     {
-        var environments = AppConfig!.Environments;
+        var environments = AppConfig.Environments;
         var environment = environments.FirstOrDefault(x => x.Identifier == identifier);
         return environment;
     }
     
     public static EnvironmentModel? FindEnvironmentByPythonPath(string pythonPath)
     {
-        var environments = AppConfig!.Environments;
+        var environments = AppConfig.Environments;
         var environment = environments.FirstOrDefault(x => pythonPath.PathEquals(x.PythonPath));
         return environment;
     }
